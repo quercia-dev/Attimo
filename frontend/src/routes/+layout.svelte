90c440e50f49902d6dbd5cf1af9a08ad9cd692ff
tferracina <tferracina@icloud.com>
2025-01-31 17:18:36 +0100

    feat: created sidebar

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,49 +1,46 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppShell, AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	let currentTile: number = 0;
 </script>
 
 <!-- App Shell -->
 <AppShell>
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
+	<!-- Sidebar -->
+	 <AppRail>
+		<AppRailTile bind:group={currentTile} name="home" value={0} title="home">
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<div style="display: flex; justify-content: center;"><img src="/home.svg" alt="Home" /></div>
 			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
+		</AppRailTile>
+		<AppRailTile bind:group={currentTile} name="calendar" value={1} title="calendar">
+			<svelte:fragment slot="lead">
+				<div style="display: flex; justify-content: center;"> <img src="/calendar.svg" alt="Calendar" /> </div>
 			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
+		</AppRailTile>
+		<AppRailTile bind:group={currentTile} name="open" value={2} title="open">
+			<svelte:fragment slot="lead">
+				<div style="display: flex; justify-content: center;"> <img src="/plus.svg" alt="Open" /> </div>
+			</svelte:fragment>
+		</AppRailTile>
+		<AppRailTile bind:group={currentTile} name="close" value={3} title="close">
+			<svelte:fragment slot="lead">
+				<div style="display: flex; justify-content: center;"> <img src="/minus.svg" alt="Close" /> </div>
+			</svelte:fragment>
+		</AppRailTile>
+		<!-- --- -->
+		<svelte:fragment slot="trail">
+			<AppRailAnchor href="/" target="_blank" title="SETTINGS">
+				<div style="display: flex; justify-content: center;"> <img src="/settings.svg" alt="Settings" /> </div></AppRailAnchor>
+		</svelte:fragment>
+		</AppRail>
 	<!-- Page Route Content -->
 	<slot />
 </AppShell>
+
