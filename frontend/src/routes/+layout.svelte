5eb8698ee92d7b80a3a885629eece3eaee4b2787
tebe-nigrelli <102675740+tebe-nigrelli@users.noreply.github.com>
2025-01-31 21:11:15 +0100

    fix: merged App Rail and App Bar

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,46 +1,74 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
 
-	// Floating UI for Popups
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
-	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	import { AppBar } from '@skeletonlabs/skeleton';
+
+	import AppRail from './AppRail.svelte';
+
+	import Home from './Home.svelte';
 
 	let currentTile: number = 0;
 </script>
 
-<!-- App Shell -->
-<AppShell>
-	<!-- Sidebar -->
-	 <AppRail>
-		<AppRailTile bind:group={currentTile} name="home" value={0} title="home">
-			<svelte:fragment slot="lead">
-				<div style="display: flex; justify-content: center;"><img src="/home.svg" alt="Home" /></div>
-			</svelte:fragment>
-		</AppRailTile>
-		<AppRailTile bind:group={currentTile} name="calendar" value={1} title="calendar">
-			<svelte:fragment slot="lead">
-				<div style="display: flex; justify-content: center;"> <img src="/calendar.svg" alt="Calendar" /> </div>
-			</svelte:fragment>
-		</AppRailTile>
-		<AppRailTile bind:group={currentTile} name="open" value={2} title="open">
-			<svelte:fragment slot="lead">
-				<div style="display: flex; justify-content: center;"> <img src="/plus.svg" alt="Open" /> </div>
-			</svelte:fragment>
-		</AppRailTile>
-		<AppRailTile bind:group={currentTile} name="close" value={3} title="close">
-			<svelte:fragment slot="lead">
-				<div style="display: flex; justify-content: center;"> <img src="/minus.svg" alt="Close" /> </div>
-			</svelte:fragment>
-		</AppRailTile>
-		<!-- --- -->
-		<svelte:fragment slot="trail">
-			<AppRailAnchor href="/" target="_blank" title="SETTINGS">
-				<div style="display: flex; justify-content: center;"> <img src="/settings.svg" alt="Settings" /> </div></AppRailAnchor>
-		</svelte:fragment>
-		</AppRail>
-	<!-- Page Route Content -->
-	<slot />
-</AppShell>
+<div style="display: flex; height: 100vh;">
+	<AppRail />
 
+	<div style="flex: 1; display: flex; flex-direction: column;">
+		<!-- AppBar -->
+		<AppBar style="grid-template-columns: 1fr 3fr;">
+			<svelte:fragment slot="lead">
+				<button
+					type="button"
+					on:click={() => console.log('click hamburger')}
+					aria-label="Open drawer"
+					class="icon-button"
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="32"
+						height="32"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="3"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						class="feather feather-menu"
+					>
+						<line x1="3" y1="12" x2="21" y2="12"></line>
+						<line x1="3" y1="6" x2="21" y2="6"></line>
+						<line x1="3" y1="18" x2="21" y2="18"></line>
+					</svg>
+				</button>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<button
+					type="button"
+					on:click={() => console.log('click hamburger')}
+					aria-label="Open drawer"
+					class="icon-button"
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="32"
+						height="32"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="3"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						class="feather feather-message-square"
+					>
+						<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+					</svg>
+				</button>
+			</svelte:fragment>
+		</AppBar>
+
+		<!-- Home Pane -->
+		<Home />
+
+		<footer class="bg-blue-500 p-1">footer</footer>
+	</div>
+</div>
