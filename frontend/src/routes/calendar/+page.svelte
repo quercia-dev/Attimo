9db2543f3fa66b1beab8a1102c412e6686baaf5b
tferracina <tferracina@icloud.com>
2025-02-03 12:43:49 +0100

    feat: added calendar views

--- frontend/src/routes/calendar/+page.svelte
+++ frontend/src/routes/calendar/+page.svelte
@@ -4,24 +4,44 @@
 
 	import Calendar from '@event-calendar/core';
 	import TimeGrid from '@event-calendar/time-grid';
+	import List from '@event-calendar/list';
+	import DayGrid from '@event-calendar/day-grid';
 
-	let plugins = [TimeGrid];
+	let plugins = [TimeGrid, List, DayGrid];
 	let options = {
-		view: 'timeGridWeek',
-		slotMinTime: '08:00:00',
-		slotMaxTime: '20:00:00',
+		headerToolbar: {
+			left: 'prev,next today',
+			center: 'title',
+			right: 'timeGridWeek,dayGridMonth,listWeek'
+		},
+		initialView: 'timeGridWeek',
+		firstDay: 1,
 		slotWidth: 10,
+		views: {
+			timeGridWeek: {
+				type: 'timeGrid',
+				duration: { weeks: 1 }
+			},
+			dayGridMonth: {
+				type: 'dayGrid',
+				duration: { months: 1 }
+			},
+			listWeek: {
+				type: 'list',
+				duration: { weeks: 1 }
+			}
+		},
 		events: [
 			{
 				title: 'Event 1',
-				start: '2025-01-29T10:00:00',
-				end: '2025-01-29T12:00:00'
+				start: '2025-02-03T10:00:00',
+				end: '2025-02-03T12:00:00'
 			}
 		]
 	};
 </script>
 
-<div class="bg-white text-gray-900 p-4 rounded-md shadow-xl flex-1 overflow-hidden">
+<div class="bg-white text-gray-900 p-4 rounded-md shadow-xl flex-1 overflow-auto">
 	<div class="calendar-container h-full w-full">
 		<Calendar {plugins} {options} />
 	</div>
