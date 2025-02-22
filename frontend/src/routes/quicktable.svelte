9defe7bc746acbf4116d6af4ecb7d60dabc984c2
tebe-nigrelli <102675740+tebe-nigrelli@users.noreply.github.com>
2025-02-22 19:54:24 +0100

    fix: simplified icon css

--- frontend/src/routes/quicktable.svelte
+++ frontend/src/routes/quicktable.svelte
@@ -4,12 +4,12 @@
 	import { showAppRail } from '../stores/appStore';
 
 	const tableArr = [
-		{ id: 1, name: 'Todo', total: 100, continuative: false, ongoing: 70, lastWeek: 50, },
-		{ id: 2, name: 'Projects', total: 80, continuative: true, ongoing: 80, lastWeek: 60, },
-		{ id: 3, name: 'People', total: 65, continuative: true, ongoing: 65, lastWeek: 40, },
-		{ id: 4, name: 'People', total: 65, continuative: true, ongoing: 65, lastWeek: 40, },
-		{ id: 5, name: 'People', total: 65, continuative: false, ongoing: 65, lastWeek: 40, },
-		{ id: 6, name: 'Tasks', total: 75, continuative: true, ongoing: 75, lastWeek: 55, }
+		{ id: 1, name: 'Todo', total: 100, continuative: false, ongoing: 70, lastWeek: 50, lastMonth: 80 },
+		{ id: 2, name: 'Projects', total: 80, continuative: true, ongoing: 80, lastWeek: 60, lastMonth: 70 },
+		{ id: 3, name: 'People', total: 65, continuative: true, ongoing: 65, lastWeek: 40, lastMonth: 50 },
+		{ id: 4, name: 'People', total: 65, continuative: true, ongoing: 65, lastWeek: 40, lastMonth: 50 },
+		{ id: 5, name: 'People', total: 65, continuative: false, ongoing: 65, lastWeek: 40, lastMonth: 50 },
+		{ id: 6, name: 'Tasks', total: 75, continuative: true, ongoing: 75, lastWeek: 55, lastMonth: 65 }
 	];
 </script>
 
@@ -24,8 +24,9 @@
 				<th></th>
 				<th>Open</th>
 				<th></th>
-				<th>Last week</th>
 				<th>Activity Graph</th>
+				<th>Last week</th>
+				<th>Last month</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -35,16 +36,16 @@
 				<td style="text-align: center;">{row.total}</td>
 
                 <td>
-                    <button
-                        type="button"
-                        onclick={() => showAppRail.update(value => !value)}
-                        aria-label="Open drawer"
-                        class="hover:bg-primary-300 dark:hover:bg-primary-900"
-                    >
-                        <svg class="feather">
-                            <use href="/icons/feather-sprite.svg#plus" />
-                        </svg>
-                    </button>
+					<button
+						type="button"
+						onclick={() => showAppRail.update(value => !value)}
+						aria-label="Open drawer"
+						class="small-icon-button bg-green-300 dark:bg-green-700"
+					>
+						<svg class="feather">
+							<use href="/icons/feather-sprite.svg#plus" />
+						</svg>
+					</button>
                 </td>
 				{#if row.continuative}
 				<td style="text-align: center;">{row.ongoing}</td>
@@ -54,7 +55,7 @@
                         type="button"
                         onclick={() => showAppRail.update(value => !value)}
                         aria-label="Open drawer"
-                        class="hover:bg-primary-300 dark:hover:bg-primary-900"
+                        class="small-icon-button bg-red-300 dark:bg-red-900"
                     >
                         <svg class="feather">
                             <use href="/icons/feather-sprite.svg#minus" />
@@ -65,9 +66,10 @@
 				<td></td>
 				<td></td>
 				{/if}
-				<td style="text-align: center;">{row.lastWeek}</td>
-				
 				<td style="text-align: center; width: 100%;">|___/\___/\_____</td>
+
+				<td style="text-align: center;">{row.lastWeek}</td>
+				<td style="text-align: center;">{row.lastMonth}</td>
 			</tr>
 			{/each}
 		</tbody>
